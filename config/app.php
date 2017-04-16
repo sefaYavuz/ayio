03ebae040eea7f512d2cd224d61bfddc7a985839
Sefa Yavuz <seff61@gmail.com>
2017-04-16 15:09:13 +0200

    Added whoops and laravel exceptions error handler

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        'GrahamCampbell\Exceptions\ExceptionsServiceProvider',
 
         /*
          * Application Service Providers...
