8a1cea2060a41ce33d6e470692ff0e9bbe3e5993
Sefa Yavuz <seff61@gmail.com>
2017-04-18 19:34:48 +0200

    Add base for creating menu items

---
+++ packages/ayio/ui/src/migrations/2017_04_18_102418_create_menu_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('url');
+            $table->integer('order');
+            $table->tinyInteger('deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('menu');
+    }
+}
