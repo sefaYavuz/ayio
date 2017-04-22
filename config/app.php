a8c7055559190d3ab5827e0afce46fb05f57b66e
Sefa Yavuz <seff61@gmail.com>
2017-04-22 23:05:33 +0200

    Added laravel packager

--- config/app.php
+++ config/app.php
@@ -179,6 +179,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        JeroenG\Packager\PackagerServiceProvider::class,
+
     ],
 
     /*
