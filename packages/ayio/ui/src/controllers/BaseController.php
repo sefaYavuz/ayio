433bc20752d512bd2b4ca141364aa497c61b60b8
Sefa Yavuz <seff61@gmail.com>
2017-04-23 21:37:13 +0200

    Added traits to basecontroller

--- packages/ayio/ui/src/controllers/BaseController.php
+++ packages/ayio/ui/src/controllers/BaseController.php
@@ -5,9 +5,14 @@ namespace Ayio\Ui\Controllers;
 use Ayio\Ui\Models\Menu;
 use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request as Controller;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class BaseController extends Controller
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
     public function __construct() {
         $menu = Menu::where('deleted', 0)
         ->orderBy('order')
