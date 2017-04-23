6988d40df0e9f7095bb5d91f0a7bf16ebd48f099
Sefa Yavuz <seff61@gmail.com>
2017-04-23 12:44:38 +0200

    Backend and frontend routes

--- packages/ayio/home/src/HomeServiceProvider.php
+++ packages/ayio/home/src/HomeServiceProvider.php
@@ -13,7 +13,8 @@ class HomeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/backend.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/frontend.php');
         $this->loadViewsFrom(__DIR__ . '/views', 'home');
     }
 
