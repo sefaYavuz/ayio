2424502e6db3f9fbdc882a9dcbe7abfa8c11b1ce
Sefa Yavuz <seff61@gmail.com>
2017-04-27 21:00:12 +0200

    Make sidebar menu dynamic

--- packages/ayio/admin/src/controllers/AdminController.php
+++ packages/ayio/admin/src/controllers/AdminController.php
@@ -3,11 +3,20 @@
 namespace Ayio\Admin\Controllers;
 
 use Ayio\Ui\Controllers\BaseController;
+use Ayio\Ui\Models\Menu;
+use Illuminate\Support\Facades\View;
 
 class AdminController extends BaseController
 {
     public function __construct() {
         $this->middleware('auth');
+
+        $menu = Menu::where('deleted', 0)
+        ->where('hidden', 0)
+        ->where('backend', 1)
+        ->orderBy('order')
+        ->get();
+        View::share('menu', $menu);
     }
 
     public function index() {
--- packages/ayio/admin/src/views/includes/sidebar.blade.php
+++ packages/ayio/admin/src/views/includes/sidebar.blade.php
@@ -1,14 +1,10 @@
 <div class="main">
-    <a href="/admin" class="item active">
-        <span class="icon">
-            <i class="fa fa-dashboard"></i>
-        </span>
-        <span class="name">Dashboard</span>
-    </a>
-    <a href="/logout" class="item">
-        <span class="icon">
-            <i class="fa fa-sign-out"></i>
-        </span>
-        <span class="name">Logout</span>
-    </a>
+    @foreach ($menu as $menuItem)
+        <a href="{{ url($menuItem->url) }}" class="item {{ in_array(Request::path(), json_decode($menuItem->actives)) ? 'active' : '' }}">
+            <span class="icon">
+                <i class="{{ $menuItem->icon }}"></i>
+            </span>
+            <span class="name">{{ $menuItem->title }}</span>
+        </a>
+    @endforeach
 </div>
