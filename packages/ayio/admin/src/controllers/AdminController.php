c9780279b56f3d9a867f63b06c1c9422b900cbc4
Sefa Yavuz <seff61@gmail.com>
2017-04-28 22:59:07 +0200

    Seperate basecontroller for admin package

--- packages/ayio/admin/src/controllers/AdminController.php
+++ packages/ayio/admin/src/controllers/AdminController.php
@@ -2,21 +2,11 @@
 
 namespace Ayio\Admin\Controllers;
 
-use Ayio\Ui\Controllers\BaseController;
-use Ayio\Ui\Models\Menu;
-use Illuminate\Support\Facades\View;
-
 class AdminController extends BaseController
 {
-    public function __construct() {
+    function __construct() {
+        parent::__construct();
         $this->middleware('auth');
-
-        $menu = Menu::where('deleted', 0)
-        ->where('hidden', 0)
-        ->where('backend', 1)
-        ->orderBy('order')
-        ->get();
-        View::share('menu', $menu);
     }
 
     public function index() {
---
+++ packages/ayio/admin/src/controllers/BaseController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ayio\Admin\Controllers;
+
+use Ayio\Ui\Models\Menu;
+use Illuminate\Routing\Controller as Controller;
+use Illuminate\Support\Facades\View;
+
+class BaseController extends Controller
+{
+    function __construct() {
+        $menuItems = Menu::where('deleted', 0)
+        ->where('hidden', 0)
+        ->where('backend', 1)
+        ->orderBy('parent_id')
+        ->orderBy('order')
+        ->get();
+
+        $menu = new Menu();
+        $menu = $menu->buildMenu($menuItems);
+
+        View::share('menu', $menu);
+    }
+}
