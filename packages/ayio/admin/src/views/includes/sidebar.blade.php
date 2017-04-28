99a0ab8e73950e8743a870b665e5801f015d1616
Sefa Yavuz <seff61@gmail.com>
2017-04-28 23:00:06 +0200

    Overhaul of menu

--- packages/ayio/admin/src/views/includes/sidebar.blade.php
+++ packages/ayio/admin/src/views/includes/sidebar.blade.php
@@ -1,10 +1,34 @@
 <div class="main">
-    @foreach ($menu as $menuItem)
-        <a href="{{ url($menuItem->url) }}" class="item {{ in_array(Request::path(), json_decode($menuItem->actives)) ? 'active' : '' }}">
-            <span class="icon">
-                <i class="{{ $menuItem->icon }}"></i>
-            </span>
-            <span class="name">{{ $menuItem->title }}</span>
-        </a>
-    @endforeach
+    <aside class="menu">
+        <ul class="menu-list">
+            @foreach ($menu as $menuItems)
+                @foreach($menuItems as $menuItem)
+                    <li>
+                    @if (!is_array($menuItem))
+
+                            <a href="{{ url($menuItem->url) }}" class="item {{ in_array(Request::path(), json_decode($menuItem->actives)) ? 'active' : '' }}">
+                                <span class="icon">
+                                    <i class="{{ $menuItem->icon }}"></i>
+                                </span>
+                                <span class="name">{{ $menuItem->title }}</span>
+                            </a>
+                    @else
+                        <ul>
+                            @foreach ($menuItem as $child)
+                                <li>
+                                    <a href="{{ url($child->url) }}" class="item {{ in_array(Request::path(), json_decode($child->actives)) ? 'active' : '' }}">
+                                        <span class="icon">
+                                            <i class="{{ $child->icon }}"></i>
+                                        </span>
+                                        <span class="name">{{ $child->title }}</span>
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    </li>
+                @endforeach
+            @endforeach
+        </ul>
+    </aside>
 </div>
