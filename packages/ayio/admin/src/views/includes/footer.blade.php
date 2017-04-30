0b7df8dc0860c4073bf66a2fbce3cd15a96a1bc5
Sefa Yavuz <seff61@gmail.com>
2017-04-30 14:34:19 +0200

    Added restful links library

--- packages/ayio/admin/src/views/includes/footer.blade.php
+++ packages/ayio/admin/src/views/includes/footer.blade.php
@@ -1,2 +1,3 @@
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="{{ asset('assets/ayio/admin/js/vendor/restful_links.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/ayio/admin/js/main.js') }}"></script>
