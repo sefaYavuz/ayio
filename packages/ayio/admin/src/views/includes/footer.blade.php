87562ca5c0dbbc353d52c3939b1c3c52c36f32cb
Sefa Yavuz <seff61@gmail.com>
2017-04-30 14:40:41 +0200

    Added froala editor library

--- packages/ayio/admin/src/views/includes/footer.blade.php
+++ packages/ayio/admin/src/views/includes/footer.blade.php
@@ -1,3 +1,4 @@
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/froala-editor/2.5.1//js/froala_editor.pkgd.min.js"></script>
 <script type="text/javascript" src="{{ asset('assets/ayio/admin/js/vendor/restful_links.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/ayio/admin/js/main.js') }}"></script>
--- packages/ayio/admin/src/views/includes/head.blade.php
+++ packages/ayio/admin/src/views/includes/head.blade.php
@@ -2,6 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Your App Title</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<link href="{{ asset('assets/ayio/ui/css/vendor/bulma.css') }}" rel="stylesheet" />
-<link href="{{ asset('assets/ayio/admin/css/base.css') }}" rel="stylesheet" />
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/froala-editor/2.5.1/css/froala_editor.pkgd.min.css" />
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/froala-editor/2.5.1/css/froala_style.min.css" />
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ asset('assets/ayio/ui/css/vendor/bulma.css') }}"  />
+<link rel="stylesheet" href="{{ asset('assets/ayio/admin/css/base.css') }}"  />
