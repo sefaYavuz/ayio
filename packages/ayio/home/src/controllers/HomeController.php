08bd4af09e24b101dfb6dd7fe7c68b6feaeed864
Sefa Yavuz <seff61@gmail.com>
2017-04-30 17:18:42 +0200

    Removed  menu worker for home package

--- packages/ayio/home/src/controllers/HomeController.php
+++ packages/ayio/home/src/controllers/HomeController.php
@@ -10,13 +10,6 @@ class HomeController extends BaseController
 {
 
     public function index() {
-        $menu = new stdClass();
-        $menu->title = 'Home';
-        $menu->slug = 'home';
-        $menu->url = '/home';
-        $menu->order = 100;
-
-        dispatch(new MenuWorker($menu));
 
         return view('home::home');
     }
